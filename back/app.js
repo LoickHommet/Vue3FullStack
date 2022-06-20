2185e556de07689c0fd5901619d2e7fab398f0d7
Loick Hommet <hommet.loick@gmail.com>
2022-06-20 15:01:12 +0200

    Créeation back signup

--- back/app.js
+++ back/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('express-async-errors');
 const Joi = require('joi');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const cors = require('cors');
@@ -20,21 +20,63 @@ const userSchema = new mongoose.Schema({
     email : String,
     password : String
 });
+
+let _id = 1;
+
 const User = mongoose.model('User', userSchema);
 
 // Connexion à MongoDB
 mongoose.connect("mongodb://localhost:27017/Eval")
 .then( async function () {
     console.log("Connecté à la BDD");
-
-    
-
   
 })
 .catch( () => {
     console.log("Non connecté")
 })
 
+
+app.post('/signup', async (req, res) => {
+    
+    const payload = req.body;
+
+    const joiUserSchema = Joi.object({
+        email : Joi.string().min(4).max(255).required(),
+        password : Joi.string().min(2).max(255).required(),
+    })
+    
+
+    const {error} = joiUserSchema.validate(payload);
+
+    if ( ! error) {
+        let id = _id++;
+
+        const userFound = await User.findOne({email : payload.email}).exec()
+            .then( async (data) => {
+                if ( ! data) {
+                    const salt = await bcrypt.genSalt(10);
+                    let { password } = payload;
+                    passwordHashed = await bcrypt.hash(password, salt);
+                    payload.password = passwordHashed;
+                    
+                    const user = new User({...payload, _id : id});
+                    await user.save();
+
+                    delete payload.password;
+                    res.status(201).send({...payload, _id : id});
+                }
+
+                else {
+                    res.send("Cette adresse est déjà associé à un compte");
+                };
+
+            })
+        ;
+    }
+    else {
+        res.status(400).send({ erreur : error.details[0].message });
+    }
+})
 
 
 if (process.env.NODE_ENV !== 'test') 
