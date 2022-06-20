8fd4cf8f5e75fad8a8745fff470f7821ef803f34
Loick Hommet <hommet.loick@gmail.com>
2022-06-20 15:06:14 +0200

    Template Formulaire d'inscription

--- front/src/components/register.vue
+++ front/src/components/register.vue
@@ -3,7 +3,35 @@
 </script>
 
 <template>
-  <h1>register</h1>
+<div style="padding: 2em;">
+    <p> {{user}} </p>
+    <form @submit.prevent="signup( mail, password)">
+          <!-- <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label ">Identifiant</label>
+        <input type="text" v-model="identifiant" class="form-control" id="exampleInputPassword1" />
+      </div> -->
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input
+          type="email"
+          v-model="mail"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+      
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      </div>
+          <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Password</label>
+      <input type="password" v-model="password" name="" id="">
+    </div>
+     
+      
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+
 </template>
 
 <style scoped>
