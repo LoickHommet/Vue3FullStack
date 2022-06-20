0c39e693a57a30666d44bc190e0aaf94ab2d6ff6
Loick Hommet <hommet.loick@gmail.com>
2022-06-20 15:17:26 +0200

    Template login

--- front/src/components/login.vue
+++ front/src/components/login.vue
@@ -3,7 +3,20 @@
 </script>
 
 <template>
-  <h1>Login</h1>
+<div style="padding: 2em;">
+    <h1>Login</h1>
+    <form @submit.prevent="">
+        <div class="form-group mb-3">
+            <label for="email">Email</label>
+            <input type="text" v-model="email" class="form-control" id="email" placeholder="Enter name">
+        </div>
+        <div class="form-group mb-3">
+            <label for="password">Password</label>
+            <input type="password" v-model="password" class="form-control" id="password" placeholder="Enter name">
+        </div>
+        <button type="submit" class="btn btn-success">Connexion</button>
+    </form>
+</div>
 </template>
 
 <style scoped>
